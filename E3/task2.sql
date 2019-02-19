ef396ac8279157b18e3032c3f8cc0139d7f4ea48
MARCUS-DESKTOP\marcu <marcusbjorna@gmail.com>
2019-02-19 15:04:44 +0100

    da

--- E3/task2.sql
+++ E3/task2.sql
@@ -9,4 +9,13 @@ select name from actor where birthyear > 1960;
 select name from actor where (birthyear between 1980 and 1989) order by name;
 
 #d
+select role, title from movie inner join actorinmovie on (movie.movieid = actorinmovie.movieid) where actorid in (select actorid from actor where name ="Morgan Freeman");
 
+#e uferdig
+# select distinct title from movie inner join director on (movie.directorid = director.directorid) where 
+
+#f
+select count(*) from actor where (lower(name) like 'c%');
+
+#g
+select genre.name, count(movie.title) from genre join 
