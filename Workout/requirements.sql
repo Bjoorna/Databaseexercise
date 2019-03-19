f72bc70534dbe7a107fdd8c9bdf608066020d0bf
Marcus Bjørnå <marcusbjorna@gmail.com>
2019-03-19 11:21:25 +0100

    siste fra prosjekt

--- Workout/requirements.sql
+++ Workout/requirements.sql
@@ -8,7 +8,10 @@ select * from equipment;
 insert into exercise(exerciseid, name, sets) values (1, "Pushups", 3);
 insert into exercise(exerciseid, name, kilo, sets) values (2, "Squat", 100, 3);
 update exercise set EquipmentID = 2 where exerciseid = 2;
-update exercise set Equipmentname = (select Equipmentname from equipment where equipmentID = 1) where exerciseid = 2;
 select * from exercise;
 
-insert into workout values (1, 2019-02-11 22:12:00, 
+insert into workout(workoutid, date, length, personalscore, performance) values (2, "2019-02-11 11:11:11", 60, 20, 8);
+select * from workout;
+
+
+
