bc5646443769dca0bc1db3b728c5584904bfa1d7
Petter Eikeland <paeikela@stud.ntnu.no>
2019-03-21 19:01:34 +0100

    Added AddExerciseGroup class. TODO: implement finding of similar exercises

---
+++ Prosjekt/Prosjekt2/src/AddExerciseGroup.java
@@ -0,0 +1,57 @@
+import java.sql.*;
+import java.util.*;
+
+public class AddExerciseGroup extends DBConn {
+
+    String EGroupName;
+    int inEGroupID;
+    String inEGroupName;
+
+
+    public void InsertExerciseGroup(){
+        try{
+            exerciseGroup = connection.createStatement();
+            String insert = "INSERT INTO exercisegroups(name) values (" + getEGroupName() +");";
+            exerciseGroup.executeUpdate(insert);
+            System.out.println(insert);
+        }catch (Exception e){
+            throw new RuntimeException("Feil i addeq", e);
+        }
+        try{
+            inExerciseGroup = connection.createStatement();
+            String insert = "INSERT INTO inexercisegroup(exerciseid, name) values (" + getEGroupName() +");";
+
+        }
+
+        try{
+            if (connection!=null){
+                connection.close();
+                System.out.println("Connection closed");
+            }
+        }catch (Exception e){
+            throw new RuntimeException("t", e);
+        }
+    }
+    public int getInEGroupID() {
+        return inEGroupID;
+    }
+    public void setEGroupName(String EGroupName) {
+        EGroupName = "\"" + EGroupName + "\"";
+        this.EGroupName = EGroupName;
+    }
+
+    public void setInEGroupID(int inEGroupID) {
+        this.inEGroupID = inEGroupID;
+    }
+    public String getEGroupName() {
+        return EGroupName;
+    }
+
+    public String getinEGroupName() {
+        return inEGroupName;
+    }
+    public void setinEGroupName(String inEGroupName) {
+        inEGroupName = "\"" + inEGroupName + "\"";
+        this.inEGroupName = inEGroupName;
+    }
+}
